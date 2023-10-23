f17923bdd6fdf9ac7eec0257009043d3343fe554
Devon <dsamson2@academic.rrc.ca>
2023-10-23 17:22:52 -0500

    added the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+# Create 676 fake products
+676.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price(range: 1..100.0, as_string: false),
+    stock_quantity: Faker::Number.between(from: 1, to: 1000)
+  )
+end
