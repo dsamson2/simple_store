fb913c0afa3b001301295e98c3cf32d1b8e6c038
Devon <dsamson2@academic.rrc.ca>
2023-10-23 18:05:19 -0500

    updated index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'items/show'
   get 'products/index'
   get '/products', to: 'product#index'
+  resources :items, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
